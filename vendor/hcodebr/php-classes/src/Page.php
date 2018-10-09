fa4f68c62127a8abea7aeb184b5d1878d8a24e45
Jeronimo-Pelicoli <jeronimosama@gmail.com>
2018-10-09 16:57:24 -0300

    tela de login

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[],
 	];
 
@@ -28,7 +30,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if($this->options["header"]===true) $this->tpl->draw("header");
 
 	}
 
@@ -46,7 +48,7 @@ class Page {
 
 	public function __destruct() {
 
-		$this->tpl->draw("footer");
+		if($this->options["footer"]===true) $this->tpl->draw("footer");
 
 	}
 }
