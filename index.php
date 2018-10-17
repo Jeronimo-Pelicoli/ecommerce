5a9237bec0b9b311bc913d9ea0089dae78a4658d
Jeronimo-Pelicoli <jeronimosama@gmail.com>
2018-10-17 18:14:34 -0300

    Paginação

--- index.php
+++ index.php
@@ -3,60 +3,17 @@ session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-use \Hcode\Model\User;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$page = new Page();
-
-	$page->setTpl("index");
-
-});
-
-$app->get('/admin', function() {
-
-	User::verifyLogin();
-    
-	$page = new PageAdmin();
-
-	$page->setTpl("index");
-
-});
-
-$app->get('/admin/login', function() {
-    
-	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false
-	]);
-
-	$page->setTpl("login");
-
-});
-
-$app->post('/admin/login', function() {
-
-	User::login($_POST["login"], $_POST["password"]);
-
-	header("Location: /admin");
-	exit;
-});
-
-$app->get('/admin/logout', function() {
-
-	User::logout();
-
-	header("Location: /admin/login");
-	exit;
-
-});
-
+require_once("functions.php");
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -36,7 +36,7 @@ class Page {
 
 	private function setData($data = array()) {
 		foreach ($data as $key => $value) {
-			$this->tpl->assing($key, $value);
+			$this->tpl->assign($key, $value);
 		}
 	}
 
